88855887f7a7e01aa7032f74381c0c419f8e6750
Arthur Chabole <83458500+Chabole@users.noreply.github.com>
2022-04-02 17:18:58 -0300

    Add dados

--- Classe_avião.py
+++ Classe_avião.py
@@ -2,8 +2,8 @@ import numpy as np
 import ZebraLib as Lzb
 
 class plane:
-    def _init_(self, Sw=1, AR=2, teta=10, cw=0.5, bw=2):
 
+    def _init_(self, Sw=1, AR=2, teta=10, cw=0.5, bw=2):
         #Asa
         self.Sw = Sw
         self.teta = teta
--- gerador_de_avião.py
+++ gerador_de_avião.py
@@ -1,8 +1,10 @@
 import numpy as np
 import ZebraLib as zb
 import time 
+import pandas as pd
 
 class Aviao():
+
     def init(self, b=1.86, S=0.843, Af=0.601, CLn=1.080, CLmax=1.5193, c=0.463):
         self.b = b
         self.S = S
@@ -17,15 +19,17 @@ class Aviao():
         self.Af = np.linspace(0.2, 1, 10000)
         self.CLn = np.linspace(1.3, 1.87, 10000)
         self.c = np.linspace(0.2, 0.8, 10000)
+
         z = list()
         for x in range(5):
             p = zb.Airplane(S=self.S[x], b=self.b[x], Af=self.Af[x], CLn=self.CLn[x], c=self.c[x])
             z.append(p)
         return z
     
-            
+df = pd.DataFrame()
+
 start = time.time()
 teste = Aviao()
 a = teste.dados()
-print(a)
+
 print(time.time() - start)
